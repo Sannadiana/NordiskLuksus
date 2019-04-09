2cecfe1ddf07e74518836324484319cf82475277
sannadianae <sanna-diana@hotmail.comm>
2019-04-09 08:20:57 +0200

    slettet

--- Models/StoreContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace NordiskLuksusMVC.Models{
-    public class StoreContext:DbContext
-    {
-        public StoreContext(DbContextOptions<StoreContext> options):base(options){}
-        public DbSet<NordiskLuksusMVC.Models.Product> Product{get; set;}
-    }
-}
